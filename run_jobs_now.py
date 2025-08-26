557a74c2a06cdca118b415fce759e8d6da510454
rsims55 <randisims55@gmail.com>
2025-08-26 14:36:24 -0400

    automated snapshot sync (2025-08-26 14:36:24)

---
+++ run_jobs_now.py
@@ -0,0 +1,18 @@
+﻿import os, sys, time
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
+from cfb_tix import daemon as d
+
+paths = d.detect_paths()
+print("==> weekly_update"); sys.stdout.flush()
+d.job_weekly_update(paths)
+
+print("==> daily_snapshot"); sys.stdout.flush()
+d.job_daily_snapshot(paths)
+
+print("==> train_price_model"); sys.stdout.flush()
+d.job_train_model(paths)
+
+print("==> predict_price"); sys.stdout.flush()
+d.job_predict_price(paths)
+
+print("==> ALL DONE"); sys.stdout.flush()
