f7d8d9bbe272adb713267c5cf22b0f0633ff6500
rsims55 <randisims55@gmail.com>
2025-08-27 09:37:38 -0400

    automated snapshot sync (2025-08-27 09:37:38)

---
+++ _train_once.py
@@ -0,0 +1,7 @@
+﻿import sys
+from pathlib import Path
+ROOT = Path(r"C:\Users\randi\GitHub Repos\football-ticket-tracker\football-ticket-tracker")
+sys.path.insert(0, str(ROOT / "src"))
+from cfb_tix import daemon as d
+p = d.detect_paths()
+d.job_train_model(p)
